4c76322b103a1678dbc731a3bbf2d41571923b09
Jhonny Mata <solucionesmatajm@gmail.com>
2017-12-03 15:04:10 -0400

    Cambiando Database a clase abstracta

--- src/Database.php
+++ src/Database.php
@@ -1,75 +1,28 @@
 <?php
 namespace phpORM;
 
-class Database{
-    protected $database;
-    protected $comands = [];
-    protected $autocomit = true;
-    public $busy = false;
-    public function __construct($settings, $username = NULL, $pass = NULL){
-        $this->conn = new \PDO($settings, $username, $pass);
-    }
+abstract class Database{
+    protected static $database;
+    protected static $comands = [];
+    public static $busy = false;
+    public static $dns;
+    public static $username;
+    public static $password;
+    public static $options;
     /**
      * Retorna la instancia PDO de la conexión
      * @return \PDO
      */
-    public function getContainer(){
-        return $this->database;
-    }
-    /**
-     * Retorna el pool de tareas pendientes por ejecutar
-     * @return Array[string]
-     */
-    public function getComands(){
-        return $this->commands;
-    }
-    /**
-     * Ejecuta directamente los comandos SQL
-     * @return \PDOStatement
-     */
-    protected function execute(){
-        try{
-            return $this->database->exec($value);
-        }catch(\PDOException $error){
-            throw new Exception\IntegrityError($error->getMessage());
+    public static function getContainer()
+    {
+        if(!self::$database){
+           $database = new \PDO(
+               self::$dns,
+               self::$username,
+               self::$password,
+               self::$options);
+            self::$database = new Schema($database);
         }
-    }
-    /**
-     * Retorna el pool de resultados de las tareas ejecutadas
-     * @return Array[\PDOStatement]
-     */
-    protected function executes(){
-        $results = [];
-        foreach($this->comands as $value){
-            $results = $this->execute($value);
-        }
-        return $results;
-    }
-    /**
-     *  Prepara una sentencia para su ejecución
-     * @return \PDOStatement
-     */
-    protected function prepare($SQL, $INPUT){
-        try{
-            $stm = $this->database-prepare($SQL);
-            $query = $stm->execute($INPUT);
-            return $stm;
-        }catch(\PDOException $error){
-            throw new Exception\IntegrityError($error->getMessage());
-        }
-    }
-    public function createTable($safe=false){
-        $SQL = $this->parseSQLTable();
-        try{
-            $this->execute($SQL);
-        }catch(Exception\IntegrityError $e){
-            if(!$safe){
-                return;
-            }
-            throw $e;
-        }
-    }
-    private function parseSQLTable(){
-        return "CREATE TABLE prueba(id int not null);";
+        return self::$database;
     }
 }
