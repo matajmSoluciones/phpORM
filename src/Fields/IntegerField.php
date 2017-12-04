0f1c8437a6d499358b1233c11d608fb99cbe20cd
Jhonny Mata <solucionesmatajm@gmail.com>
2017-12-03 20:56:46 -0400

    Manejo de strings en formato para Integer en clase IntegerField

--- src/Fields/IntegerField.php
+++ src/Fields/IntegerField.php
@@ -4,4 +4,14 @@ namespace phpORM\Fields;
 class IntegerField extends BaseField{
     protected $column_type = "INT";
     protected $column_size = 8;
+    public function __toString(){
+        $SQL = "{$this->db_column} {$this->column_type}";
+        if(!$this->null){
+            $SQL.= " NOT NULL";
+        }
+        if(!$this->optional){
+            $SQL.= $this->optional;
+        }
+        return $SQL;
+    }
 }
