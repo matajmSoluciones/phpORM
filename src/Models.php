dcf9647749f73f150d2f2123c08868ed8de627c0
Jhonny Mata <solucionesmatajm@gmail.com>
2017-12-08 11:53:31 -0400

    añadido validacion booleano campo primary key en clase Models

--- src/Models.php
+++ src/Models.php
@@ -53,11 +53,11 @@ abstract class Models{
             if (isset($column["null"])) {
                 $db_null = $column["null"];
             }
-            if (isset($column["primary_key"])) {
+            if (isset($column["primary_key"]) && $column["primary_key"] == true) {
                 $db_null = false;
             }
             $fieldClass = new $column["type"]($MSB, $db_column, $db_size, $db_null);
-            if (isset($column["primary_key"])) {
+            if (isset($column["primary_key"]) && $column["primary_key"] = true) {
                 $constraint = new Fields\PrimaryKey(uniqid(), $db_column);
                 $fieldClass->addConstraints($constraint);
                 static::$pk_primary[] = $constraint;
