913df51363e0d8b121084ec66f70106dced4cbf5
Jhonny Mata <solucionesmatajm@gmail.com>
2017-12-14 12:13:14 -0400

    añadido soporte TextField

---
+++ src/Fields/TextField.php
@@ -0,0 +1,17 @@
+<?php
+namespace phpORM\Fields;
+
+class TextField extends BaseField{
+    protected $column_type = "INT";
+    protected $column_size;
+    public function __toString(){
+        $SQL = "{$this->db_column} {$this->column_type}";
+        if(!$this->null){
+            $SQL.= " NOT NULL";
+        }
+        if(!$this->optional){
+            $SQL.= " ".$this->optional;
+        }
+        return $SQL;
+    }
+}
